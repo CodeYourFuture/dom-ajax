36b8ae60ce20d194f009a01ad951e6a50547bb93
YJohn <yfekadu2016@gmail.com>
2017-06-20 18:22:28 +0100

    create a restriction for the forms

--- js/homework.js
+++ js/homework.js
@@ -2,7 +2,7 @@ var bluChange = document.querySelector('#blueBtn');
 // select the button and assign to the bluChange variable
 bluChange.addEventListener('click', changeBlueColor);
 // create an Event and call the function
-function changeBlueColor(){
+function changeBlueColor() {
     var myJumbo = document.querySelector('.jumbotron');
     var myDonate = document.querySelector('.btn-primary')
     var volnter = document.querySelector('.btn-secondary')
@@ -16,7 +16,7 @@ var oraChange = document.querySelector('#orangeBtn');
 // select the button and assign to the oraChange variable
 oraChange.addEventListener('click', changeOrangeColor);
 // create an Event and call the function
-function changeOrangeColor(){
+function changeOrangeColor() {
     //change the color of selected query
     var myJumbo = document.querySelector('.jumbotron');
     var myDonate = document.querySelector('.btn-primary')
@@ -31,7 +31,7 @@ var greenChange = document.querySelector('#greenBtn');
 // select the button and assign to the greenChange variable
 greenChange.addEventListener('click', changeGreenColor);
 // create an Event and call the function
-function changeGreenColor(){
+function changeGreenColor() {
     var myJumbo = document.querySelector('.jumbotron');
     var myDonate = document.querySelector('.btn-primary')
     var volnter = document.querySelector('.btn-secondary')
@@ -41,5 +41,38 @@ function changeGreenColor(){
 }
 
 
-var register = document.querySelector('.summitValid');
-register.
+var register = document.querySelector('#summitValid');
+register.addEventListener('click', validatFormFild);
+
+
+function validatFormFild() {
+    event.preventDefault();
+    var userName = document.querySelector('#example-text-input');
+    var emailAddress = document.querySelector('#exampleInputEmail1');
+    var textArea = document.querySelector('#exampleTextarea');
+    if (!userName.value) {
+        alert('please Enter your user name?');
+        userName.style.backgroundColor = 'red';
+    } else if (!emailAddress.value) {
+        alert('please Enter a valid Email Address?');
+        emailAddress.style.backgroundColor = 'red';
+    }
+    else if (!textArea.value) {
+        alert('please discribe about yourself?');
+        textArea.style.backgroundColor = 'red';
+    } else if (textArea.value && emailAddress && textArea) {
+        // 1. clear input
+        alert('thank you for filling out the form');
+        userName.value = ' ';
+        emailAddress = ' ';
+        textArea = ' ';
+    } else {
+        userName.value = ' ';
+        emailAddress = ' ';
+        textArea = ' ';
+        userName.style.backgroundColor = ' ';
+        emailAddress.style.backgroundColor = ' ';
+        textArea.style.backgroundColor = ' ';
+    }
+
+}
