30abe6aac78bf4ef4a90448e7d324e9468f1db37
YJohn <yfekadu2016@gmail.com>
2017-06-22 09:31:22 +0100

    reduce complexity of the code

--- js/homework.js
+++ js/homework.js
@@ -1,40 +1,31 @@
-var bluChange = document.querySelector('#blueBtn');
-// select the button and assign to the bluChange variable
-bluChange.addEventListener('click', changeBlueColor);
-// create an Event and call the function
+var bluChange = document.querySelector('#blueBtn');// select the button and assign to the bluChange variable
+bluChange.addEventListener('click', changeBlueColor);// create an Event and call the function
+
+var oraChange = document.querySelector('#orangeBtn');
+oraChange.addEventListener('click', changeOrangeColor);
+
+var greenChange = document.querySelector('#greenBtn');
+greenChange.addEventListener('click', changeGreenColor);
+
+var myJumbo = document.querySelector('.jumbotron');
+var myDonate = document.querySelector('.btn-primary')
+var volnter = document.querySelector('.btn-secondary')
+
 function changeBlueColor() {
-    var myJumbo = document.querySelector('.jumbotron');
-    var myDonate = document.querySelector('.btn-primary')
-    var volnter = document.querySelector('.btn-secondary')
     myJumbo.style.backgroundColor = '#588fbd';
     myDonate.style.backgroundColor = '#ffa500';
     volnter.style.backgroundColor = 'black';
     volnter.style.color = 'white';
 }
 
-var oraChange = document.querySelector('#orangeBtn');
-// select the button and assign to the oraChange variable
-oraChange.addEventListener('click', changeOrangeColor);
-// create an Event and call the function
 function changeOrangeColor() {
-    //change the color of selected query
-    var myJumbo = document.querySelector('.jumbotron');
-    var myDonate = document.querySelector('.btn-primary')
-    var volnter = document.querySelector('.btn-secondary')
     myJumbo.style.backgroundColor = '#f0ad4e';
     myDonate.style.backgroundColor = '#5751fd';
     volnter.style.backgroundColor = '#31b0d5';
     volnter.style.color = 'white';
 }
 
-var greenChange = document.querySelector('#greenBtn');
-// select the button and assign to the greenChange variable
-greenChange.addEventListener('click', changeGreenColor);
-// create an Event and call the function
 function changeGreenColor() {
-    var myJumbo = document.querySelector('.jumbotron');
-    var myDonate = document.querySelector('.btn-primary')
-    var volnter = document.querySelector('.btn-secondary')
     myJumbo.style.backgroundColor = '#87ca8a';
     myDonate.style.backgroundColor = 'black';
     volnter.style.backgroundColor = '#8c9c08';
@@ -51,14 +42,11 @@ function validatFormFild() {
     var emailAddress = document.querySelector('#exampleInputEmail1');
     var textArea = document.querySelector('#exampleTextarea');
     var isEmail = false;
-    for (i = 0; i < emailAddress.value.length; i++) {
-        if (emailAddress.value[i] === '@') {
-            isEmail = true;
+    if (emailAddress && emailAddress.value.indexOf('@') !== -1) {
+        isEmail = true;
 
-        }
     }
-
-    if (!isEmail) {
+    else if (!isEmail) {
         alert('please Enter a valid Email Address');
         emailAddress.style.backgroundColor = 'red';
 
