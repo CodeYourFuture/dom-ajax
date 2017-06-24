8cf63d1b591b1a8e6635f7dffe60d4ec6ef74248
panditita <topimorita@gmail.com>
2017-06-24 19:01:45 +0100

    Buttons funtion correctly and form has especific alerts and blanks out after been sucessfully filled

---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+
+var myButton = document.querySelector("#donateBike");
+
+myButton.addEventListener("click", doSomething);
+
+function doSomething() {
+    var box = document.querySelector(".jumbotron");
+    if (!box.className.includes("red")) {
+        box.className += " red";
+    }
+}
+
+var myButton2 = document.querySelector("#addToLearnMore"); // Element that is going to call the action
+
+myButton2.addEventListener("click", doSomething2);
+
+function doSomething2() {
+    var box = document.querySelector("#mainArticles"); // Call the element that want to change
+    var paragraph = document.createElement("article.article");
+    paragraph.style.color = "blue";
+    paragraph.innerText = "SHOW ME MORE JAVASCRIPT!!";
+
+    box.appendChild(paragraph);
+}
+
+var images = document.querySelector("#allImages"); // Element that is going to call the action
+images.addEventListener("click", selectAll);
+
+function selectAll() {
+    var allImages = document.querySelectorAll("img");
+    for (var i = 0; i < allImages.length; i++) {
+        allImages[i].width = allImages[i].width - 10;
+        allImages[i].height = allImages[i].height - 10;
+
+    }
+};
+
+var divs = document.querySelector("#addToLearnMore"); // Element that is going to call the action
+divs.addEventListener("click", selectDivs);
+
+function selectDivs() {
+    var allDivs = document.querySelectorAll("div");
+    if (!allDivs.className.includes("pink")) {
+        allDivs.className += " pink";
+    }
+}
+
+// AJAX
+//creating a request object
+
+var addInfo = document.querySelector("#addArticleBtn"); // Element that is going to call the action
+addInfo.addEventListener("click", request);
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    // var messageBox = document.querySelector(".form-control addArticle");
+    if (request.readyState === 4) {  // check if a response was sent back
+        if (request.status === 200) {     // check if request was successful
+            document.querySelector("#textBox").value = JSON.parse(request.responseText)["message"]; // needs to be value because it's an input object in htlm
+        } else {
+            requestMessage.innerHTML = 'An error occurred during your request: ' + request.status + ' ' + request.statusText;
+        }
+    }
+}
+var url = "http://ajax-cyf.eu-west-1.elasticbeanstalk.com/chatroom/?id=c";                                        //server location
+request.open("GET", url);                    // adding it to the request
+
+request.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //header info
+request.send();                                 // sending the request
